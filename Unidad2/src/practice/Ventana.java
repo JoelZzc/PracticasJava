e47d16eb2f49e1ecddaf9512111176158333e7d1
SEARS <SEARS@LAPTOP-OV12DLMI>
2025-02-19 09:06:01 -0700

    unidad 2 ejercicio 14 o diseño de registro

--- Unidad2/src/practice/Ventana.java
+++ Unidad2/src/practice/Ventana.java
@@ -1,13 +1,18 @@
 package practice;
 
 import java.awt.Color;
+
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
@@ -20,16 +25,20 @@ public class Ventana extends JFrame{
 		this.setLocationRelativeTo(null);
 		this.setResizable(true);
 		
-	
-		JPanel login= new JPanel();
-		login.setVisible(true);
-		login.setLocation(0,0);
-		login.setSize(500,500);
-		login.setOpaque(true);
-		login.setLayout(null);
-		
-		this.add(login);
+		this.add(this.Registro());
+		//this.add(this.login());
 		this.repaint();
+	}
+	
+	public JPanel login () {
+		
+		JPanel panelLogin= new JPanel();
+		panelLogin.setVisible(true);
+		panelLogin.setLocation(0,0);
+		panelLogin.setSize(500,500);
+		panelLogin.setOpaque(true);
+		panelLogin.setLayout(null);
+		
 		
 		
 		JLabel etiqueta1 = new JLabel("Inicia sesión");
@@ -37,49 +46,142 @@ public class Ventana extends JFrame{
 		etiqueta1.setLocation(160,10);
 		etiqueta1.setFont(new Font("Arial",Font.BOLD,25));
 		etiqueta1.setHorizontalAlignment(JLabel.CENTER);
-		login.add(etiqueta1);
+		panelLogin.add(etiqueta1);
 		
 		JLabel etiqueta2 = new JLabel("Nombre de usuario ");
 		etiqueta2.setSize(180,25);
 		etiqueta2.setLocation(30,100);
 		etiqueta2.setFont(new Font("Arial",Font.BOLD,15));
-		login.add(etiqueta2);
+		panelLogin.add(etiqueta2);
 		
 		JTextField nombreUsuario = new JTextField();
 		nombreUsuario.setBounds(230, 100, 180,25);
 		nombreUsuario.setText("Nombre de usuario..." );
-		login.add(nombreUsuario);
+		panelLogin.add(nombreUsuario);
 		
 		JLabel contraseña = new JLabel("Contraseña ");
 		contraseña.setSize(180,25);
 		contraseña.setLocation(30,160);
 		contraseña.setFont(new Font("Arial",Font.BOLD,15));
-		login.add(contraseña);
+		panelLogin.add(contraseña);
 		
 		JTextField contraseña2 = new JTextField();
 		contraseña2.setBounds(230, 160, 180,25);
 		contraseña2.setText("Contraseña..." );
-		login.add(contraseña2);
+		panelLogin.add(contraseña2);
 		
 		JCheckBox checkBox1 = new JCheckBox("Recordar mi contraseña");
         checkBox1.setBounds(30,220,180,25);
-        login.add(checkBox1);
+        panelLogin.add(checkBox1);
 		
         JLabel olvideContraseña = new JLabel("¿Olvidaste tu contraseña?");
         olvideContraseña.setBounds(230,220,180,25);
         olvideContraseña.setFont(new Font("Arial",Font.BOLD,12));
-		login.add(olvideContraseña);
+        panelLogin.add(olvideContraseña);
 		
 		JButton iniciar = new JButton("Siguiente");
 		iniciar.setBounds(230,280,180,25);
 		iniciar.setFont(new Font("Arial",Font.BOLD,12));
-		login.add(iniciar);
+		panelLogin.add(iniciar);
 		
 		JButton crearCuenta = new JButton("Crear cuenta");
 		crearCuenta.setBounds(30,280,180,25);
 		crearCuenta.setFont(new Font("Arial",Font.BOLD,12));
-		login.add(crearCuenta);
-	
+		panelLogin.add(crearCuenta);
 		
+		this.repaint();
+		
+		return panelLogin;
 	}	
+	
+	public JPanel Registro() {
+		
+		JPanel panelRegistro = new JPanel();
+		panelRegistro.setVisible(true);
+		panelRegistro.setLocation(0,0);
+		panelRegistro.setSize(500,500);
+		panelRegistro.setOpaque(true);
+		panelRegistro.setLayout(null);
+		
+		
+		JLabel etiqueta1 = new JLabel("Registro");
+		etiqueta1.setSize(160,40);
+		etiqueta1.setLocation(160,10);
+		etiqueta1.setFont(new Font("Arial",Font.BOLD,25));
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER);
+		panelRegistro.add(etiqueta1);
+		
+		JLabel etiqueta2 = new JLabel("Nombre de usuario ");
+		etiqueta2.setSize(180,25);
+		etiqueta2.setLocation(30,100);
+		etiqueta2.setFont(new Font("Arial",Font.BOLD,15));
+		panelRegistro.add(etiqueta2);
+		
+		JTextField nombreUsuario = new JTextField();
+		nombreUsuario.setBounds(230, 100, 230,25);
+		nombreUsuario.setText("Nombre de usuario..." );
+		panelRegistro.add(nombreUsuario);
+		
+		JLabel bio = new JLabel("BIO ");
+		bio.setSize(180,25);
+		bio.setLocation(30,160);
+		bio.setFont(new Font("Arial",Font.BOLD,15));
+		panelRegistro.add(bio);
+		
+		JTextField bio2 = new JTextField();
+		bio2.setBounds(230, 140, 230,50);
+		bio2.setText("BIO..." );
+		panelRegistro.add(bio2);
+		
+		JLabel preferencias = new JLabel("Preferencias");
+		preferencias.setSize(180,25);
+		preferencias.setLocation(30,220);
+		preferencias.setFont(new Font("Arial",Font.BOLD,15));
+		panelRegistro.add(preferencias);
+		
+		JCheckBox dulces = new JCheckBox("Dulces");
+		dulces.setBounds(30,260,90,25);
+        panelRegistro.add(dulces);
+        
+        JCheckBox salado = new JCheckBox("Salado");
+        salado.setBounds(130,260,90,25);
+        panelRegistro.add(salado);
+        
+        JCheckBox saludable = new JCheckBox("Saludable");
+        saludable.setBounds(230,260,90,25);
+        panelRegistro.add(saludable);
+        
+        JLabel términos1 = new JLabel("Términos");
+        términos1.setBounds(30,300,180,25);
+        términos1.setFont(new Font("Arial",Font.BOLD,15));
+        panelRegistro.add(términos1);
+			
+		JButton crearCuenta = new JButton("Crear cuenta");
+		crearCuenta.setBounds(150,430,180,25);
+		crearCuenta.setFont(new Font("Arial",Font.BOLD,12));
+		panelRegistro.add(crearCuenta);
+		
+		String [] colonias_dataset= {"centro", "Villas del encanto", "pedregal", "Agua escondida","Balandra", "Camino real"};
+		JComboBox colonias = new JComboBox(colonias_dataset);
+		colonias.setBounds(30, 380, 180, 25);
+		panelRegistro.add(colonias);
+		
+		ButtonGroup terminos= new ButtonGroup();
+		
+		JRadioButton terms1= new JRadioButton("Acepto los terminos");
+		terms1.setBounds(30, 330, 180,40);
+		panelRegistro.add(terms1);
+		
+		JRadioButton terms2= new JRadioButton("No acepto los terminos");
+		terms2.setBounds(250, 330, 180,40);
+		panelRegistro.add(terms2);
+		
+		terminos.add(terms1);
+		terminos.add(terms2);
+
+				
+		return panelRegistro;
+	}
+	
+	
 }
