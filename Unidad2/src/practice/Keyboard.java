5dc6d8b708c2c550927c896aae49d54099459d73
SEARS <SEARS@LAPTOP-OV12DLMI>
2025-04-08 18:42:36 -0700

    "Unidad 3 - Ejercicio 29: Movimiento y colisiones con obstáculos"

--- Unidad2/src/practice/Keyboard.java
+++ Unidad2/src/practice/Keyboard.java
@@ -20,6 +20,7 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.awt.Color;
@@ -31,7 +32,8 @@ public class Keyboard extends JFrame implements KeyListener {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private PaintPanel panel;
-	public int x=200,y=200;
+	private Player player;
+	private ArrayList<Player> obstaculos = new ArrayList<Player>();
 
 	/**
 	 * Launch the application.
@@ -60,6 +62,11 @@ public class Keyboard extends JFrame implements KeyListener {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
+		player = new Player(200,200,25,25,Color.ORANGE);
+		
+		obstaculos.add(new Player(150,350, 220, 30,Color.RED));
+		obstaculos.add(new Player(150,50, 220, 30,Color.RED));
+
 		
 		panel = new PaintPanel();
 		panel.setBackground(new Color(124, 195, 235));
@@ -77,8 +84,8 @@ public class Keyboard extends JFrame implements KeyListener {
 		JButton btnNewButton = new JButton("Reiniciar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				x=200;
-				y=200;
+				player.x=200;
+				player.y=200;
 				panel.repaint();
 				panel.requestFocus();
 			}
@@ -100,9 +107,17 @@ public class Keyboard extends JFrame implements KeyListener {
  	       super.paintComponent(g);
  	       
  	       Graphics2D g2 = (Graphics2D) g; 
-		  ImageIcon img = new ImageIcon("C:\\Users\\SEARS\\git\\PracticasJava\\Unidad2\\src\\practice\\astronave.png");
-		  Image imagen=img.getImage();		
-		  g2.drawImage(imagen, x, y, 64,64,null);
+		  /*ImageIcon img = new ImageIcon("C:\\Users\\SEARS\\git\\PracticasJava\\Unidad2\\src\\practice\\astronave.png");
+		  Image imagen=img.getImage();	
+		  g2.drawImage(imagen, player.x, player.y, player.w,player.h,null);*/	
+		  g2.setColor(player.c);
+		  g2.fillRect(player.x,player.y, player.w, player.h);
+		  
+		  
+		  for (Player pared : obstaculos) {
+			  g2.setColor(Color.red);
+			  g2.fillRect(pared.x,pared.y, pared.w, pared.h);
+		}
 
 
  	   }
@@ -119,26 +134,52 @@ public class Keyboard extends JFrame implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println(e.getKeyCode());
+		Boolean m=true;
+		for (Player pared : obstaculos) {
+			if(player.colision(pared))
+				m=false;
+		}
 		
 		if(e.getKeyCode()==87) {
-			y-=10;
-			if (y<0)
-				y=panel.getHeight();
+			
+			if(m) {
+				player.y-=10;
+				if (player.y<0)
+					player.y=panel.getHeight();
+			}else {
+				player.y+=10;
+			}
+
 		}
 		if(e.getKeyCode()==83) {
-			y+=10;
-			if (y>panel.getHeight())
-				y=0;
+			
+			if(m) {
+				player.y+=10;
+				if (player.y>panel.getHeight())
+					player.y=0;
+			}else {
+				player.y-=10;
+			}
+		
 		}
 		if(e.getKeyCode()==65) {
-			x-=10;
-			if (x<0)
-				x=panel.getWidth();
+			if(m) {
+				player.x-=10;
+				if (player.x<0)
+					player.x=panel.getWidth();
+			}else {
+				player.x+=10;
+			}
+			
 		}
 		if(e.getKeyCode()==68) {
-			x+=10;
-			if (x>panel.getWidth())
-				x=0;
+			if(m) {
+				player.x+=10;
+				if (player.x>panel.getWidth())
+					player.x=0;
+			}else {
+				player.x-=10;
+			}
 		}
 		
 		  panel.repaint();
@@ -150,5 +191,23 @@ public class Keyboard extends JFrame implements KeyListener {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	class Player{
+		int x=0,y=0,w=0,h=0;
+		Color c=Color.orange;
+		public Player(int x, int y, int w, int h, Color c) {
+			this.x=x;
+			this.y=y;
+			this.w=w;
+			this.h=h;					
+			
+		}
+		
+		public Boolean colision(Player target){
+			return (this.x < target.x + target.w && 
+					this.x + this.w > target.x &&
+	                this.y < target.y + target.h &&
+	                this.y + this.h > target.y);
+		}
+	}
 }
