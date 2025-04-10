298a576b5c36d4017163e2c8db17f4fea7adb90e
SEARS <SEARS@LAPTOP-OV12DLMI>
2025-04-10 13:23:04 -0700

    Unidad 3 - Ejercicio 30: Mejorando el movimiento y colisiones con obstáculos

--- Unidad2/src/practice/Keyboard.java
+++ Unidad2/src/practice/Keyboard.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 
 import practice.PaintJava.Figura;
@@ -32,7 +33,9 @@ public class Keyboard extends JFrame implements KeyListener {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private PaintPanel panel;
-	private Player player;
+	private Player player,shadow;
+	private Timer timer2,timer;
+	private int lastpress;
 	private ArrayList<Player> obstaculos = new ArrayList<Player>();
 
 	/**
@@ -63,7 +66,8 @@ public class Keyboard extends JFrame implements KeyListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		player = new Player(200,200,25,25,Color.ORANGE);
-		
+		shadow = new Player(200,200,25,25,Color.ORANGE);
+
 		obstaculos.add(new Player(150,350, 220, 30,Color.RED));
 		obstaculos.add(new Player(150,50, 220, 30,Color.RED));
 
@@ -77,7 +81,7 @@ public class Keyboard extends JFrame implements KeyListener {
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.NORTH);
 		
-		JLabel lblNewLabel = new JLabel("00:00");
+		JLabel lblNewLabel = new JLabel("0:0");
 		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 		panel_1.add(lblNewLabel);
 		
@@ -86,12 +90,49 @@ public class Keyboard extends JFrame implements KeyListener {
 			public void actionPerformed(ActionEvent e) {
 				player.x=200;
 				player.y=200;
+				shadow.x=200;
+				shadow.y=200;
+				
+				timer.stop();
+				lblNewLabel.setText("0:0");
+				timer2.stop();
 				panel.repaint();
 				panel.requestFocus();
 			}
 		});
 		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 18));
 		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		
+		ActionListener taskPerformer= new ActionListener(){
+			 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String[] partes = lblNewLabel.getText().split(":");
+		        int segundos = Integer.parseInt(partes[0]);
+		        int mil = Integer.parseInt(partes[1]);
+
+		        mil++;
+
+		        if (mil >= 10) {
+		        	mil = 0;
+		        	segundos++;
+		        }
+
+		        lblNewLabel.setText(segundos + ":" + mil);				
+			}
+		};
+			timer= new Timer(100,taskPerformer);
+		
+		ActionListener movimiento= new ActionListener(){
+			 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				update();
+			}
+		};
+			timer2= new Timer(10,movimiento);
+			
+		
 	}
 	
 	
@@ -106,10 +147,7 @@ public class Keyboard extends JFrame implements KeyListener {
  	   public void paintComponent(Graphics g) {
  	       super.paintComponent(g);
  	       
- 	       Graphics2D g2 = (Graphics2D) g; 
-		  /*ImageIcon img = new ImageIcon("C:\\Users\\SEARS\\git\\PracticasJava\\Unidad2\\src\\practice\\astronave.png");
-		  Image imagen=img.getImage();	
-		  g2.drawImage(imagen, player.x, player.y, player.w,player.h,null);*/	
+ 	       Graphics2D g2 = (Graphics2D) g; 	
 		  g2.setColor(player.c);
 		  g2.fillRect(player.x,player.y, player.w, player.h);
 		  
@@ -133,59 +171,74 @@ public class Keyboard extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println(e.getKeyCode());
+	    timer.start();
+		
+		timer2.start();
+		lastpress=e.getKeyCode();
+		update();
+	}
+	
+	public void update() {
 		Boolean m=true;
 		for (Player pared : obstaculos) {
 			if(player.colision(pared))
 				m=false;
 		}
 		
-		if(e.getKeyCode()==87) {
+		if(m) {
+			shadow.x=player.x;
+			shadow.y=player.y;
+		}
+		if(lastpress==87) {
 			
 			if(m) {
 				player.y-=10;
 				if (player.y<0)
 					player.y=panel.getHeight();
 			}else {
-				player.y+=10;
+				player.y=shadow.y;
+				player.x=shadow.x;
 			}
 
 		}
-		if(e.getKeyCode()==83) {
+		if(lastpress==83) {
 			
 			if(m) {
 				player.y+=10;
 				if (player.y>panel.getHeight())
 					player.y=0;
 			}else {
-				player.y-=10;
+				player.y=shadow.y;
+				player.x=shadow.x;
 			}
 		
 		}
-		if(e.getKeyCode()==65) {
+		if(lastpress==65) {
 			if(m) {
 				player.x-=10;
 				if (player.x<0)
 					player.x=panel.getWidth();
 			}else {
-				player.x+=10;
+				player.y=shadow.y;
+				player.x=shadow.x;
 			}
 			
 		}
-		if(e.getKeyCode()==68) {
+		if(lastpress==68) {
 			if(m) {
 				player.x+=10;
 				if (player.x>panel.getWidth())
 					player.x=0;
 			}else {
-				player.x-=10;
+				player.y=shadow.y;
+				player.x=shadow.x;
 			}
 		}
 		
 		  panel.repaint();
-		
 	}
 
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
